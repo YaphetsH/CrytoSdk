8134f7b642c5f6017c77933803507cf451d1776f
YaphetsH <yaphetsh@outlook.com>
2020-10-23 19:20:47 +0800

    init

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+#include "openssl/crypto.h"
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_yaphetsh_qhcipher_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(OpenSSL_version(OPENSSL_VERSION));
+}
